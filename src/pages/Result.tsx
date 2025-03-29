c129936dc116f67be751aa4f0e5bf31be1b28ca3
LinaMobarak <linamubark7@gmail.com>
2025-03-29 15:20:36 +0400

    HiSalem

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -11,13 +11,13 @@ const Result = () => {
 
     <div className='flex flex-col items-center justify-center'>
         <h1 className='text-4xl font-bold'>Quiz Completed</h1>
-        <h2 className='text-2xl font-semibold'>Your Score:</h2>
-        <p className='text-xl'>{useQuizStore.getState().score}</p>
-
+        <h2 className='text-2xl font-semibold'>Your Score: {useQuizStore.getState().score}</h2>
+        {/* <p className='text-xl'>{useQuizStore.getState().score}</p> */}
+        
         {allQuestions.map((question, index) => (
             <div key={index} className='border p-4 m-2 w-1/2'>
                 <h3 className='text-lg font-semibold'>{question.question}</h3>
-                <p className='text-sm'>Correct Answer: {question.answer}</p>
+                <p style={{backgroundColor: 'green'}}className='text-sm'>Correct Answer: {question.answer}</p>
                 <p className='text-sm'>Your Answer: {userAnswers[index]}</p>
             </div>
         ))}
